e824387d3f188f4f43e9f46a6e57889710fa7d48
6331667 <6331667@cegepmontpetit.ca>
2025-08-26 21:52:46 -0400

    Update exercice2.2.ps1

--- R02 - Cmdlets et objets/exercice2.2.ps1
+++ R02 - Cmdlets et objets/exercice2.2.ps1
@@ -10,38 +10,39 @@
 Get-ChildItem
 
 # 2. Obtenir la liste des fichiers seulement contenus directement sous C:\Windows.
-
+Get-ChildItem -Path C:\Windows -File
 
 # 3. Obtenir la liste des répertoires seulement contenus directement sous C:\Program Files.
-
+Get-ChildItem -Path C:\"Program Files" -Directory
 
 # 4. Obtenir la liste des processus actifs dont le nom commence par "V" (vous pouvez utiliser les wildcards).
-
+Get-Process *V
 
 # 5. Obtenir de l'information sur le volume C: (comme sa taille, l'espace restant, le format du système de fichiers, etc.)
-
+Get-Volume -DriveLetter C
 
 # 6. Créer un nouveau répertoire nommé Minou à la racine du volume C:\.
-
+New-Item -Path C:\ -ItemType Directory -Name Minou
 
 # 7. Définir ce répertoire comme répertoire de travail (répertoire de travail, équivalent à cd C:\Minou).
-
+Set-Location -Path C:\Minou
 
 # 8. Obtenir la liste des adapteurs réseau installés sur l'ordinateur.
-
+Get-NetAdapter
 
 # 9. Obtenir la liste des utilisateurs locaux configurés sur l'ordinateur.
-
+Get-LocalUser -Verbose
 
 # 10. Obtenir l'adresse IP numérique du serveur www.cegepmontpetit.ca.
-
+$ip = Test-NetConnection www.cegepmontpetit.ca
+$ip.SourceAddress
 
 # 11. Vérifier si le serveur www.cegepmontpetit.ca répond sur le port 443 à l'aide de Test-NetConnection.
-
+Test-NetConnection www.cegepmontpetit.ca -Port 443
 
 # 12. Obtenez la date (et l'heure) de maintenant.
-
+Get-Date
 
 # 13. <DÉFI> Obtenez la date et l'heure dans le format suivant: 2025-08-26 14:55. Consultez l'aide en ligne (Get-Help -Online) pour plus de détails sur l'utilisation de cette commande.
-
+Get-Date -Format "yyyy/MM/dd HH:mm"
 
