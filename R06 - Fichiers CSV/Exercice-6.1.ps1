544162de87658376c93c6894e47246c2cba858f8
vcarrier <vincent.carrier@cegepmontpetit.ca>
2025-09-11 16:05:57 -0400

    Exercice 6

---
+++ R06 - Fichiers CSV/Exercice-6.1.ps1
@@ -0,0 +1,27 @@
+﻿## Exercice sur les fichiers CSV
+##
+## Voir: https://info.cegepmontpetit.ca/3t5-automatisation/exercices/fichiers-csv
+##
+
+
+# 1. Dans PowerShell, utilisez la commande Get-Content pour voir le contenu de ce fichier texte.
+
+
+
+# 2. Utilisez maintenant la commande Import-Csv pour importer le fichier sous forme d'un tableau d'objets.
+
+
+
+
+# 3. Donnez le prénom et le nom des contrebandiers, en ordre alphabétique de nom.
+
+
+
+
+# 4. Sauvegardez ces nouvelles données dans le fichier contrebandiers.csv afin qu'il puisse être ouvert dans Excel
+#    (donc encore attention au délimiteur. Attention aussi à la première ligne qui contient l'information de type (si 
+#    vous utilisez Windows PowerShell). Le fichier devrait pouvoir être ouvert dans Excel sans erreur.
+
+
+
+
