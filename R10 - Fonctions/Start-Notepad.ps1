42962d14874b61b606cf97d1c0ab9abc21a63995
6331667 <6331667@cegepmontpetit.ca>
2025-09-26 10:23:13 -0400

    Update Start-Notepad.ps1

--- R10 - Fonctions/Start-Notepad.ps1
+++ R10 - Fonctions/Start-Notepad.ps1
@@ -1,1 +1,15 @@
-﻿
+﻿function Start-Notepad {
+    param (
+        [ValidateScript({Test-Path $_})]
+        [string]$Chemin
+    )
+    if ($null -eq $Chemin) {
+        notepad.exe
+    }
+    else {
+        notepad.exe $Chemin
+    }
+
+    return Get-Process -Name Notepad
+    
+}
