7e5a4616766a82c2b10343f466584f323dbc9706
6331667 <6331667@cegepmontpetit.ca>
2025-09-26 10:40:51 -0400

    Update Pile d'appel.ps1

--- R10 - Fonctions/Pile d'appel.ps1
+++ R10 - Fonctions/Pile d'appel.ps1
@@ -1,9 +1,34 @@
-﻿function MaFonction ([int]$x, [int]$y) {
-    $z = $x + $y
-    return $z
+﻿function X ([int]$x) {
+    $xx = $x + 80
+    return $xx
+}
+function Y ([int]$y) {
+    $yy = X -x ($y * 3)
+    return $yy
 }
 
-$a = 3
-$b = 5
-$c = MaFonction $a $b
+$a = X -x 10
+$b = Y -y 17
+$c = $b - $a + 1
 Write-Host "La réponse est $c"
+
+<#
+    Trace du code
+
+    Ligne           Effet                       Pile d'appel
+    =====           =====                       ============
+
+    $a = X -x 10    Appel X avec $x=10;         Fonction X
+                    additione 80 et $x;         Script
+                    retourne 90;
+                    $a = 90
+
+    $b = Y -y 17    Appel Y avec $y=17;         Fonction Y
+                    Appel X avec $x = $Y*3;     Script
+                    retourne 131;
+                    $b = 131
+
+    $c = $b-$a +1   $a:90; $b:131; $c:42        script
+
+    Write-Host".."  Écrit "La reponse et 42"    script
+#>
