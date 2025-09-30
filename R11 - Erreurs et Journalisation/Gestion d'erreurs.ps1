cb26fbe35aac6a2f2b6e48e98db6f2cd63645258
6331667 <6331667@cegepmontpetit.ca>
2025-09-30 17:01:00 -0400

    Update Gestion d'erreurs.ps1

--- R11 - Erreurs et Journalisation/Gestion d'erreurs.ps1
+++ R11 - Erreurs et Journalisation/Gestion d'erreurs.ps1
@@ -1,10 +1,13 @@
 param (
     [Parameter(Mandatory)]
+    [ValidateCount(1,6)]
     [string[]] $Name
 )
 
+
+
 for ($i = 0; $i -lt $Name.Count; $i++) {
-    Get-LocalUser -Name $Name[$i] | Out-Null
+    Get-LocalUser -Name $Name[$i] -ErrorAction SilentlyContinue | Out-Null
     if ($?) {
         Write-Host ("L'utilisateur {0} existe" -f ("$Name"))
     }
