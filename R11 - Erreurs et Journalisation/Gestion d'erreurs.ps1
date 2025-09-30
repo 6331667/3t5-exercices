df2dbe20ce72e340086546a5fdfa9e46f497d4d4
6331667 <6331667@cegepmontpetit.ca>
2025-09-30 17:18:09 -0400

    Update Gestion d'erreurs.ps1

--- R11 - Erreurs et Journalisation/Gestion d'erreurs.ps1
+++ R11 - Erreurs et Journalisation/Gestion d'erreurs.ps1
@@ -1,17 +1,20 @@
 param (
-    [ValidateCount(1,6)]
+    [Parameter(Mandatory)]
     [string[]] $Name
 )
 
-
-
-for ($i = 0; $i -lt $Name.Count; $i++) {
-    Get-LocalUser -Name $Name[$i] -ErrorAction SilentlyContinue | Out-Null
-    if ($?) {
-        Write-Host ("L'utilisateur {0} existe" -f ("$Name"))
-    }
-    else {
-        Write-Host ("L'utilisateur {0} n'existe pas" -f ("$Name"))
-    }
+try {
+    for ($i = 0; $i -lt $Name.Count; $i++) {
+        Get-LocalUser -Name $Name[$i] -ErrorAction SilentlyContinue | Out-Null
+        if ($?) {
+            Write-Host ("L'utilisateur {0} existe" -f ($Name[$i]))
+        }
+        else {
+            Write-Host ("L'utilisateur {0} n'existe pas" -f ($Name[$i]))
+        }
+}
+}
+catch {
+    <#Do this if a terminating exception happens#>
 }
 
