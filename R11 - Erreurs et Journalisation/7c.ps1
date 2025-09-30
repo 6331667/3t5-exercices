b86d82bd82f8065ef563cf22dbcd2b41b1c8696c
6331667 <6331667@cegepmontpetit.ca>
2025-09-30 17:34:12 -0400

    Create 7c.ps1

---
+++ R11 - Erreurs et Journalisation/7c.ps1
@@ -0,0 +1,10 @@
+﻿$hour = Get-Date -Format hh
+$min = Get-Date -Format mm 
+
+
+if ($min%2 -ne 0) {
+    Write-Host ("Il est {0} heures et {1} minutes" -f $hour, $min)
+    throw "Les minutes sont impaires!"
+}
+
+Write-Host ("Il est {0} heures et {1} minutes" -f $hour, $min)
